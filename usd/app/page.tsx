58f9f081a44b85a7da57b35830814a23ec8ddc2b
FOA-developer <flourishagbanoma@gmail.com>
2026-07-14 17:01:05 +0100

    feat: Update Home component to render chart and title

--- usd/app/page.tsx
+++ usd/app/page.tsx
@@ -1,7 +1,10 @@
-
+import Chart from "./components/Chart";
 
 export default function Home() {
   return (
-    <></>
+    <div>
+       <h1>XAUUSD Dashoard</h1>
+       <Chart />
+    </div>
   );
 }
