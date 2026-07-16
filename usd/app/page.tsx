8f8027f476b296b6c14defe6bc2cfb3694fdc323
FOA-developer <flourishagbanoma@gmail.com>
2026-07-16 16:44:04 +0100

    feat: Refactor layout in Home component for improved styling and structure

--- usd/app/page.tsx
+++ usd/app/page.tsx
@@ -2,9 +2,10 @@ import Chart from "./components/Chart";
 
 export default function Home() {
   return (
-    <div>
-       <h1>XAUUSD Dashoard</h1>
-       <Chart />
+    <div className="flex h-screen flex-col bg-[#0a0e17] px-4 py-4 sm:px-6 sm:py-6">
+      <div className="mx-auto flex w-full max-w-5xl min-h-0 flex-1 flex-col">
+        <Chart />
+      </div>
     </div>
   );
 }
